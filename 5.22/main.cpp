2b11a3866c9f9b4aee3b50945fb367cffba4fbc2
Hiroyko <edwin.haffner@heig-vd.ch>
2022-11-23 16:52:33 +0100

    prout

--- 5.22/main.cpp
+++ 5.22/main.cpp
@@ -4,9 +4,17 @@
 using namespace std;
 
 int main() {
-
-    const array<string, 7> pays = {"Canada","Chine","Allemagne","Corée","Japon","Russie",
+    enum class           Pays {CANADA, CHINE, ALLEMAGNE, COREE, JAPON, RUSSIE, ETATS_UNIS};
+    const unsigned short NB_PAYS = 7;
+    const string         PAYS[] = {"Canada","Chine","Allemagne","Corée","Japon","Russie",
                                 "Etats-Unis"};
+
+    enum class TypeMedaille {OR, ARGENT, BRONZE};
+    const unsigned short NB_TYPES_MEDAILLE = 3;
+    const string TYPE_MEDAILLE[] = {"Or", "Argent", "Bronze"};
+
+    using Medaille_Obtenues = array<unsigned short, NB_TYPES_MEDAILLE>;
+
 
     const array<array<int, 3>, 7> medailles = {{{1,0,1},
                                           {1,1,0},
@@ -20,5 +28,7 @@ int main() {
 }
 
 void medaille(const array<string, 7> &pays,const array<array<int, 3>, 7> &arrMedaille){
+
+    cout << "Pour le " <<
 
 }
